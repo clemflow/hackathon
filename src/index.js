de4e526cc755b6bce1a9f3f206a723f57c3da8de
Clement Florac <clement.florac@docusign.com>
2019-02-06 07:33:14 -0800

    test

--- src/index.js
+++ src/index.js
@@ -33,8 +33,7 @@ webhookHandler.on('push', async function (repo, data) {
     for (const filePath of allTrigeredFile) {
         console.log(filePath)
         let response = await axios.get('https://api.github.com/repos/'+ data.repository.full_name +'/contents/' + filePath, config);
-        splitter.getFromBetween.get(response.data, "<$","$>");
-        codeToSave = _.concat(codeToSave, response.data.split(/[<$$>]/));
+        codeToSave = _.concat(codeToSave, splitter.getFromBetween.get(response.data, "<$","$>"));
         //.log(res);
         // .then(response => {
         //     //console.log(response.data);
