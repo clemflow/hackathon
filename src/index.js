f29f5cf79f5de4e7cdd94e4c072600f9d2825c8d
Clement Florac <clement.florac@docusign.com>
2019-02-06 06:08:03 -0800

    debug

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 let GithubWebHook = require('express-github-webhook');
 let webhookHandler = GithubWebHook({ path: '/', secret: 'azerty' });
 let bodyParser = require('body-parser');
-let base64 = require('js-base64').Base64;
+let _ = require('lodash');
 
 const axios = require('axios');
 
@@ -16,26 +16,29 @@ webhookHandler.on('push', function (repo, data) {
     //console.log(data);
     //console.log(data.head_commit);
     //console.log(data.head_commit.modified);
-
+    //console.log(data.head_commit.added);
+    let allTrigeeredFile = _.concat(data.head_commit.modified, data.head_commit.added);
+    console.log(allTrigeeredFile);
 
     console.log("push triggered !!");
+
+    allTrigeeredFile.forEach( function(element) {
+        console.log(element);
+    });
+
     let config = {
         headers: {
             accept:  'application/vnd.github.VERSION.raw',
         }
     };
 
-    axios.get('https://api.github.com/repos/'+ data.repository.full_name +'/contents/src/index.js', config)
-        .then(response => {
-            console.log(response);
-            console.log(response.data.explanation);
-            let contents = base64.decode(response.content);
-            //console.log(response.content);
-            //console.log(contents);
-        })
-        .catch(error => {
-            console.log(error);
-        });
+    // axios.get('https://api.github.com/repos/'+ data.repository.full_name +'/contents/src/index.js', config)
+    //     .then(response => {
+    //         console.log(response.data);
+    //     })
+    //     .catch(error => {
+    //         console.log(error);
+    //     });
 
 });
 
