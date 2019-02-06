8516da926ec6b78f7e78c7065c81baa74beb69b4
Clement Florac <clement.florac@docusign.com>
2019-02-06 05:41:05 -0800

    add axios

--- src/index.js
+++ src/index.js
@@ -4,16 +4,31 @@ const app = express();
 let GithubWebHook = require('express-github-webhook');
 let webhookHandler = GithubWebHook({ path: '/', secret: 'azerty' });
 let bodyParser = require('body-parser');
+let base64 = require('js-base64').Base64;
+
+const axios = require('axios');
 
 
 app.use(bodyParser.json());
 app.use(webhookHandler);
 
 webhookHandler.on('push', function (repo, data) {
-    console.log(data);
-    console.log(data.head_commit);
-    console.log(data.head_commit.modified);
+    //console.log(data);
+    //console.log(data.head_commit);
+    //console.log(data.head_commit.modified);
     console.log("push triggered !!");
+
+    axios.get('https://api.github.com/repos/clemflow/hackathon/contents/src/index.js')
+        .then(response => {
+            console.log(response.data);
+            //console.log(response.data.explanation);
+            var contents = base64.decode(res.content);
+
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
 });
 
 webhookHandler.on('*', function (event, repo, data) {
