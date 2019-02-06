36e3c89d6841a2ee1a302a3bf3a9636dd2427a34
Clement Florac <clement.florac@docusign.com>
2019-02-06 07:33:57 -0800

    test

--- src/index.js
+++ src/index.js
@@ -29,7 +29,6 @@ webhookHandler.on('push', async function (repo, data) {
         }
     };
 
-
     for (const filePath of allTrigeredFile) {
         console.log(filePath)
         let response = await axios.get('https://api.github.com/repos/'+ data.repository.full_name +'/contents/' + filePath, config);
@@ -71,8 +70,4 @@ app.listen(3000, function () {
 });
 
 
-/*
-*
-* dedededede
-*
-* */
+
