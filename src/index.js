0c2b144e5995e20b5f4848c4d2a36a1dc96bc306
Clement Florac <clement.florac@docusign.com>
2019-02-06 21:32:50 -0800

    cors fix

--- src/index.js
+++ src/index.js
@@ -44,6 +44,12 @@ webhookHandler.on('error', function (err, req, res) {
     console.log(err)
 });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/test', function(req, res) {
     res.send('hello world');
     let myobj = { tag: ['c#', 'mongo'], description: "some description.",
